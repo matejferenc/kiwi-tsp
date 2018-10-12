f1e2c4a6a7e511aae001e0d4a044e5eda3454ee9
matej ferenc <matej.ferenc@gmail.com>
2018-10-12 19:18:43 +0200

    evaluator and validator

--- src/main/scala/salesman/Evaluator.scala
+++ src/main/scala/salesman/Evaluator.scala
@@ -2,6 +2,8 @@ package salesman
 
 import java.io.FileInputStream
 
+import salesman.Main.{Flight, Problem}
+
 object Evaluator {
 
   def main(args: Array[String]): Unit = {
@@ -11,19 +13,39 @@ object Evaluator {
   }
 
   def evaluateTestCase(number: Int): Unit = {
-//    println(s"evaluating dataset $number")
+    println(s"evaluating dataset $number")
     val inputStream = new FileInputStream(getClass.getResource(s"/$number.in").getPath)
-    System.setIn(inputStream)
     val start = System.currentTimeMillis
-    Salesman.processInput()
+    val problem = Main.processInput(inputStream)
     val readingPhase = System.currentTimeMillis
     println("reading phase: " + (readingPhase - start) + " ms")
-    println()
-    Salesman.solve()
+    val solution = Main.solve(problem)
     val calculationPhase = System.currentTimeMillis
     println("calculation phase: " + (calculationPhase - readingPhase) + " ms")
-    println()
     println("total time: " + (calculationPhase - start) + " ms")
-//    inputStream.close()
+    Validator.check(problem, solution)
+    println()
+  }
+}
+
+object Validator {
+  def check(problem: Problem, solution: List[Flight]): List[String] = {
+
+    def firstAirport = if (solution.head.from != problem.start) Some("Solution should start at given airport") else None
+    def lastAirPort = if (problem.areas(problem.start).contains(solution.last.to)) Some("Solution should end at given area") else None
+    def numberOfFlights = if (solution.size != problem.areaCount) Some("Wrong number of flights") else None
+    def continuity = if (!solution.map(_.to).zip(solution.tail.map(_.from)).forall { case (to, from) => to == from })
+      Some("Arrival and departure should be always from same city")
+    else None
+    def areas = {
+      val visitedAreas = solution.map(_.to).map(problem.areas).map(_.mkString(" ")).groupBy(identity).mapValues(_.size)
+      val visitedAreasSet = visitedAreas.keys.toSet
+      val totalAreasSet = problem.areas.values.map(_.mkString(" ")).toSet
+      if (visitedAreas.values.exists(_ != 1) || visitedAreasSet != totalAreasSet)
+        Some(s"Each area should be visited just once. Offending areas: visited multiple times ${visitedAreas.filter { case (k, v) => v != 1}}, not visited at all: ${totalAreasSet -- visitedAreasSet}")
+      else None
+    }
+
+    List(firstAirport, lastAirPort, numberOfFlights, continuity, areas).flatten
   }
 }
--- src/main/scala/salesman/Salesman.scala
+++ src/main/scala/salesman/Salesman.scala
@@ -1,53 +1,93 @@
 package salesman
 
-import scala.io.StdIn
+import java.io.{BufferedReader, InputStream, InputStreamReader}
 
-object Salesman {
+object Main {
 
-  case class Flight(source: String, destination: String, day: Int, cost: Int)
+  case class Flight(from: String, to: String, day: Int, price: Int)
+  type FlightMap = Map[(Int, String), Seq[Flight]]
+  case class Problem(areaCount: Int, start: String, areas: Map[String, Seq[String]], flights: FlightMap)
   
-  var numberOfAreas: Int = _
-  var sourceAirport: String = _
-  var sameAreaAirportsByAirport: Map[String, Seq[String]] = _
-  var flightsPerDayAndAirport: Map[(Int, String), List[Flight]] = _
+  var reader: BufferedReader = _
 
   def main(args: Array[String]): Unit = {
-    processInput()
-    solve()
+    writeSolution(solve(processInput(System.in)))
+  }
+
+  def readHeader(): (Int, String) = {
+    val firstLine = reader.readLine()
+    val splittedLine = firstLine.split(" ")
+    val n = splittedLine(0).toInt
+    val source = splittedLine(1)
+    (n, source)
+  }
+
+  def readAreasWithCities(n: Int): Map[String, List[String]] = {
+    var cityToCitiesInArea = Map[String, List[String]]()
+    for (i <- 1 to n) {
+      val areaName = reader.readLine()
+      val airports = reader.readLine().split(" ").toList
+      for (airport <- airports) cityToCitiesInArea = cityToCitiesInArea + (airport -> airports)
+    }
+    cityToCitiesInArea
+  }
+
+  def readFlights(totalDays: Int): List[Flight] = {
+    var flightDefinitions = List[Flight]()
+    var line = ""
+    while ( {
+      line = reader.readLine()
+      line != null && line != ""
+    }) {
+      val flightLine = line.split(" ")
+      val from = flightLine(0)
+      val to = flightLine(1)
+      val price = flightLine(3).toInt
+      val day = flightLine(2).toInt
+      if (day == 0) {
+        val flightForEveryDay = for (day <- List.range(1, totalDays + 1)) yield Flight(from, to, day, price)
+        flightDefinitions = flightForEveryDay ::: flightDefinitions
+      } else {
+        val flight = Flight(from, to, day, price)
+        flightDefinitions = flight :: flightDefinitions
+      }
+    }
+    flightDefinitions
   }
   
-  def processInput(): Unit = {
-    val (numberOfAreas, source) = readHeader()
-    this.numberOfAreas = numberOfAreas
-    this.sourceAirport = source
-
-    sameAreaAirportsByAirport = readAreasWithCities(numberOfAreas)
-
-    val flightDefinitions: List[Flight] = readFlights(numberOfAreas)
-
-    flightsPerDayAndAirport = flightDefinitions
-      .groupBy(flight => (flight.day, flight.source))
-      .map(element => element._1 -> element._2.sortBy(_.cost))
+  def processInput(inputStream: InputStream): Problem = {
+    reader = new BufferedReader(new InputStreamReader(inputStream))
+    val (numberOfAreas, sourceAirport) = readHeader()
+    val sameAreaAirportsByAirport = readAreasWithCities(numberOfAreas)
+    val flightDefinitions = readFlights(numberOfAreas)
+    val flightsPerDayAndAirport = flightDefinitions
+      .groupBy(flight => (flight.day, flight.from))
+      .map(element => element._1 -> element._2.sortBy(_.price))
       .withDefaultValue(Nil)
+      .asInstanceOf[FlightMap]
+    Problem(numberOfAreas, sourceAirport, sameAreaAirportsByAirport, flightsPerDayAndAirport)
   }
   
-  def solve(): Unit = {
-    val solution = nearestNeighborWrapper(sourceAirport, numberOfAreas, sameAreaAirportsByAirport, flightsPerDayAndAirport)
-    println(solution.foldLeft(0)((subTotalCost, flight) => subTotalCost + flight.cost))
-    solution.foreach(flight => println(flight.source, flight.destination, flight.cost, flight.day))
+  def writeSolution(solution: List[Flight]): Unit = {
+    println(solution.foldLeft(0)((subTotalCost, flight) => subTotalCost + flight.price))
+    solution.foreach(flight => println(flight.from, flight.to, flight.price, flight.day))
+  }
+  
+  def solve(problem: Problem): List[Flight] = {
+    nearestNeighborWrapper(problem.start, problem.areaCount, problem.areas, problem.flights)
   }
 
   def nearestNeighborWrapper(startAirport: String,
                              numberOfAreas: Int,
                              sameAreaAirportsByAirport: Map[String, Seq[String]],
-                             flightsPerDayAndAirport: Map[(Int, String), List[Flight]]
+                             flightsPerDayAndAirport: Map[(Int, String), Seq[Flight]]
                             ): List[Flight] = {
     def nearestNeighbor(day: Int, visitedAirports: Set[String], currentAirport: String, path: List[Flight]): List[Flight] = {
       val lastDay = day == numberOfAreas
       val destinationAirportsFilter: Flight => Boolean = if (lastDay) {
-        flight: Flight => flight.destination == startAirport
+        flight: Flight => flight.to == startAirport
       } else {
-        flight: Flight => !visitedAirports.contains(flight.destination)
+        flight: Flight => !visitedAirports.contains(flight.to)
       }
       val availableFlights = flightsPerDayAndAirport((day, currentAirport))
         .filter(destinationAirportsFilter)
@@ -62,7 +102,7 @@ object Salesman {
         val airportsVisitedAfterThisFlight = visitedAirports ++ sameAreaAirportsByAirport(currentAirport)
 
         availableFlights.toStream
-          .map(flight => nearestNeighbor(day + 1, airportsVisitedAfterThisFlight, flight.destination, flight :: path))
+          .map(flight => nearestNeighbor(day + 1, airportsVisitedAfterThisFlight, flight.to, flight :: path))
           .find(_.nonEmpty)
           .getOrElse(Nil)
         
@@ -70,47 +110,6 @@ object Salesman {
     }
 
     nearestNeighbor(1, Set(), startAirport, List()).reverse
-  }
-
-  def readHeader(): (Int, String) = {
-    val firstLine = StdIn.readLine()
-    val splittedLine = firstLine.split(" ")
-    val n = splittedLine(0).toInt
-    val source = splittedLine(1)
-    (n, source)
-  }
-
-  def readAreasWithCities(n: Int): Map[String, List[String]] = {
-    var cityToCitiesInArea = Map[String, List[String]]()
-    for (i <- 1 to n) {
-      val areaName = StdIn.readLine()
-      val airports = StdIn.readLine().split(" ").toList
-      for (airport <- airports) cityToCitiesInArea = cityToCitiesInArea + (airport -> airports)
-    }
-    cityToCitiesInArea
-  }
-
-  def readFlights(totalDays: Int): List[Flight] = {
-    var flightDefinitions = List[Flight]()
-    var line = ""
-    while ( {
-      line = StdIn.readLine()
-      line != null && line != ""
-    }) {
-      val flightLine = line.split(" ")
-      val source = flightLine(0)
-      val destination = flightLine(1)
-      val cost = flightLine(3).toInt
-      val day = flightLine(2).toInt
-      if (day == 0) {
-        val flightForEveryDay = for (day <- List.range(1, totalDays)) yield Flight(source, destination, day, cost)
-        flightDefinitions = flightForEveryDay ::: flightDefinitions
-      } else {
-        val flight = Flight(source, destination, day, cost)
-        flightDefinitions = flight :: flightDefinitions
-      }
-    }
-    flightDefinitions
   }
 
 }
